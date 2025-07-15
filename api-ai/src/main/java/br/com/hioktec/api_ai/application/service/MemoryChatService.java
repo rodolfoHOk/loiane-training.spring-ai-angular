d9d498c6b4b609fe797d368effcf5429c440d6a5
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-07-15 14:34:25 -0300

    feat: api: config log advisor and limit messages in chat memory

--- api-ai/src/main/java/br/com/hioktec/api_ai/application/service/MemoryChatService.java
+++ api-ai/src/main/java/br/com/hioktec/api_ai/application/service/MemoryChatService.java
@@ -3,7 +3,10 @@ package br.com.hioktec.api_ai.application.service;
 import br.com.hioktec.api_ai.application.dto.ErrorResponse;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.retry.NonTransientAiException;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +15,23 @@ public class MemoryChatService {
 
     private final ChatClient chatClient;
 
-    public MemoryChatService(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public MemoryChatService(ChatClient.Builder chatClientBuilder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(10)
+                .build();
+
         this.chatClient = chatClientBuilder
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                        new SimpleLoggerAdvisor())
                 .build();
     }
 
     public String memoryChat(String message) {
         try {
             return this.chatClient.prompt()
+                    .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, "123456"))
                     .user(message)
                     .call()
                     .content();
