866374a3831a0ec87a18d4b5c818ffae79d890d0
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-07-15 15:59:59 -0300

    feat: api: add send message to specific chat endpoint

--- api-ai/src/main/java/br/com/hioktec/api_ai/infrastructure/web/chat/MemoryChatController.java
+++ api-ai/src/main/java/br/com/hioktec/api_ai/infrastructure/web/chat/MemoryChatController.java
@@ -3,6 +3,7 @@ package br.com.hioktec.api_ai.infrastructure.web.chat;
 import br.com.hioktec.api_ai.application.dto.ChatMessage;
 import br.com.hioktec.api_ai.application.dto.NewChatResponse;
 import br.com.hioktec.api_ai.application.service.MemoryChatService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class MemoryChatController {
         this.memoryChatService = memoryChatService;
     }
 
-    @PostMapping
-    ChatMessage simpleChat(@RequestBody ChatMessage message) {
-        String response = this.memoryChatService.chat(message.message(), "To do");
+    @PostMapping("/{chatId}")
+    ChatMessage simpleChat(@PathVariable String chatId, @RequestBody ChatMessage message) {
+        String response = this.memoryChatService.chat(message.message(), chatId);
         return new ChatMessage(response);
     }
 
